fe57fdad39c802a12fc68d1279677f26511c57f9
jhjalison01 <alison01@naver.com>
2023-01-11 15:43:54 +0900

    feat: 내역 화면 레이아웃 완성, 해당 날짜의 거래 내역 화면 레이아웃
    feat: 내역 화면 레이아웃 완성, 해당 날짜의 거래 내역 화면 레이아웃 완성

---
+++ client/app/src/main/java/com/example/client/DateRecordActivity.kt
@@ -0,0 +1,63 @@
+package com.example.client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.contentcapture.DataRemovalRequest
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.client.databinding.ActivityDateRecordBinding
+import com.example.client.databinding.ActivityRecordBinding
+
+class DateRecordActivity : AppCompatActivity() {
+
+    //스크롤 오류 --> 수정하기
+
+    private val data=listOf(
+        RecordData("배달의 민족", "11:00","10000원","철수랑 배달","@drawable/ic_category_food",
+            "17일",1),
+        RecordData("배달의 민족", "11:00","10000원","철수랑 배달","@drawable/ic_category_food",
+            "17일",1),
+        RecordData("배달의 민족", "11:00","10000원","철수랑 배달","@drawable/ic_category_food",
+            "17일",1),
+        RecordData("배달의 민족", "11:00","10000원","철수랑 배달","@drawable/ic_category_food",
+            "17일",1),
+        RecordData("배달의 민족", "11:00","10000원","철수랑 배달","@drawable/ic_category_food",
+            "17일",1),
+        RecordData("배달의 민족", "11:00","10000원","철수랑 배달","@drawable/ic_category_food",
+            "17일",1),
+        RecordData("배달의 민족", "11:00","10000원","철수랑 배달","@drawable/ic_category_food",
+            "17일",1),
+        RecordData("배달의 민족", "11:00","10000원","철수랑 배달","@drawable/ic_category_food",
+            "17일",1),
+
+    )
+
+    private val data2=listOf(
+
+        RecordData("배달의 민족", "11:00","10000원","철수랑 배달","@drawable/ic_category_food",
+            "17일",1),
+        RecordData("배달의 민족", "11:00","10000원","철수랑 배달","@drawable/ic_category_food",
+            "17일",1),
+        RecordData("배달의 민족", "11:00","10000원","철수랑 배달","@drawable/ic_category_food",
+            "17일",1),RecordData("배달의 민족", "11:00","10000원","철수랑 배달","@drawable/ic_category_food",
+            "17일",1),
+    )
+
+
+
+    private lateinit var viewBinding: ActivityDateRecordBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding=ActivityDateRecordBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.lvExpense.layoutManager=LinearLayoutManager(this)
+        viewBinding.lvExpense.adapter=DateRecordAdapter(data)
+
+        viewBinding.lvIncome.layoutManager=LinearLayoutManager(this)
+        viewBinding.lvIncome.adapter=DateRecordAdapter(data2)
+
+        val decoration = ItemDecoration(20)
+        viewBinding.lvExpense.addItemDecoration(decoration)
+        viewBinding.lvIncome.addItemDecoration(decoration)
+    }
+}
