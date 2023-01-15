fbad808b2bbc53dff8be000f5aab10a0e64a49aa
MinJung <52921222+Minjungh63@users.noreply.github.com>
2023-01-15 19:19:04 +0900

    chore: changeCategory 초기값 제거

--- client/app/src/main/java/com/example/client/ui/category/ChangeCategoryActivity.kt
+++ client/app/src/main/java/com/example/client/ui/category/ChangeCategoryActivity.kt
@@ -33,41 +33,6 @@ class ChangeCategoryActivity : AppCompatActivity() {
 
         if(roomDb != null){
             categoryList = roomDb.CategoryDao().selectByTypeId(categoryType)
-            val categoryLength = categoryList.size
-            if(categoryLength == 0){
-                roomDb.CategoryDao().insert(Category("하루세끼", R.drawable.ic_category_food,1,0,false))
-                roomDb.CategoryDao().insert(Category("주거·통신",
-                    R.drawable.ic_category_living,1,1,false))
-                roomDb.CategoryDao().insert(Category("교통관련",
-                    R.drawable.ic_category_traffic,1,2,false))
-                roomDb.CategoryDao().insert(Category("생필품", R.drawable.ic_category_market,1,3,false))
-                roomDb.CategoryDao().insert(Category("나를 위한",
-                    R.drawable.ic_category_formeexpense,1,4,false))
-                roomDb.CategoryDao().insert(Category("선물준비",
-                    R.drawable.ic_category_present,1,5,false))
-                roomDb.CategoryDao().insert(Category("자기계발",
-                    R.drawable.ic_category_selfimprovement,1,6,false))
-                roomDb.CategoryDao().insert(Category("카페·간식", R.drawable.ic_category_cafe,1,7,false))
-                roomDb.CategoryDao().insert(Category("저축", R.drawable.ic_category_saving,1,8,false))
-                roomDb.CategoryDao().insert(Category("술·외식", R.drawable.ic_category_alchol,1,9,false))
-                roomDb.CategoryDao().insert(Category("의료·건강",
-                    R.drawable.ic_category_medical,1,10,false))
-                roomDb.CategoryDao().insert(Category("오락·취미",
-                    R.drawable.ic_category_entertainment,1,11,false))
-                roomDb.CategoryDao().insert(Category("여행", R.drawable.ic_category_travel,1,12,false))
-                roomDb.CategoryDao().insert(Category("자산이동",
-                    R.drawable.ic_category_assetmovement,1,13,false))
-                roomDb.CategoryDao().insert(Category("기타지출",
-                    R.drawable.ic_category_others,1,14,false))
-                roomDb.CategoryDao().insert(Category("애견용품", R.drawable.ic_category_user,1,15,true))
-                roomDb.CategoryDao().insert(Category("회비", R.drawable.ic_category_user,1,16,true))
-                roomDb.CategoryDao().insert(Category("수입", R.drawable.ic_category_income,2,0,false))
-                roomDb.CategoryDao().insert(Category("장학금",
-                    R.drawable.ic_category_income_user,2,1,true))
-                roomDb.CategoryDao().insert(Category("이자",
-                    R.drawable.ic_category_income_user,2,2,true))
-            }
-
             adapter = CategoryViewAdapter(this, categoryList, selectedCategoryPosition)
             viewBinding.changeCategoryList.adapter = adapter
             viewBinding.changeCategoryList.choiceMode = GridView.CHOICE_MODE_SINGLE
