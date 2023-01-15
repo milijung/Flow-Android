a773fe522c8543302449e48f66b954b62680d31c
MinJung <52921222+Minjungh63@users.noreply.github.com>
2023-01-15 19:17:50 +0900

    chore: AndroidManifest Activity 추가

--- client/app/src/main/java/com/example/client/SplashActivity.kt
+++ client/app/src/main/java/com/example/client/SplashActivity.kt
@@ -11,7 +11,7 @@ import com.example.client.ui.navigation.BottomNavigationActivity
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent(this, ChangeCategoryActivity::class.java)
+        val intent = Intent(this, BottomNavigationActivity::class.java)
         startActivity(intent)
         finish()
     }
