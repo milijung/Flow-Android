740b0db91da9714199dccf606fb95b0b9ddba0ef
leeseoyoung0822 <822angel@naver.com>
2023-01-16 12:55:44 +0900

    feat : 설정 레이아웃, 예산설정 레이아웃 (미완)

---
+++ client/app/src/main/java/com/example/client/SettingBankAppChoiceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingBankAppChoiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_bank_app_choice)
+    }
+}
---
+++ client/app/src/main/java/com/example/client/SettingLetterAddInputActivity.kt
@@ -0,0 +1,11 @@
+package com.example.client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingLetterAddInputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_letter_add_input)
+    }
+}
