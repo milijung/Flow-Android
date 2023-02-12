405f02113c2f15b4cddfbc55a98a430fabb577a9
leeseoyoung0822 <822angel@naver.com>
2023-02-12 17:39:36 +0900

    feat : 초기화, 탈퇴 api

--- client/app/src/main/java/com/example/client/ui/modal/SettingUserDeleteModal.kt
+++ client/app/src/main/java/com/example/client/ui/modal/SettingUserDeleteModal.kt
@@ -4,24 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.client.api.HttpConnection
 import com.example.client.data.AppDatabase
-import com.example.client.databinding.ModalSettingInitializationBinding
+import com.example.client.databinding.ModalSettingUserDeleteBinding
 import kotlinx.coroutines.InternalCoroutinesApi
 
-class SettingInitializationModal : AppCompatActivity() {
-    private lateinit var binding: ModalSettingInitializationBinding
+class SettingUserDeleteModal : AppCompatActivity() {
+    private lateinit var binding :  ModalSettingUserDeleteBinding
     private val httpConnection = HttpConnection()
 
     @OptIn(InternalCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ModalSettingInitializationBinding.inflate(layoutInflater)
+        binding  = ModalSettingUserDeleteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val roomDatabase = AppDatabase.getInstance(this)
         val userId = roomDatabase!!.UserDao().getUserId()
 
-        binding.modalDelete.setOnClickListener {
+        binding.userDeleteBtn.setOnClickListener {
             httpConnection.allDataDelete(this, userId)
         }
+
     }
 }
--- client/app/src/main/java/com/example/client/ui/setting/Response.kt
+++
@@ -1,9 +0,0 @@
-package com.example.client.ui.setting
-
-
-data class Response(
-    val isSuccess : Boolean,
-    val code : Int,
-    val message : String,
-    val result : String
-)
